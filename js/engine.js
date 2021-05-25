f81fd90f5629f9a60adc31e85293e0f60ae114cf
Nathan Pham <nathanpham.me@gmail.com>
2021-05-25 10:41:07 -0700

    rotations

--- js/engine.js
+++ js/engine.js
@@ -2,4 +2,8 @@ import Canvas from "./classes/Canvas.js"
 
 const canvas = new Canvas({ target: "#canvas" })
 
+for(const [key, value] of Object.entries(glMatrix)) {
+    window[key] = value
+}
+
 export default canvas
--- js/index.js
+++ js/index.js
@@ -11,11 +11,13 @@ const fragmentShader = await new ShaderLoader("FragmentShader", gl.FRAGMENT_SHAD
 
 const simpleShader = new ShaderLinker(vertexShader, fragmentShader).link()
 const vertexPosition = simpleShader.getLocation("attribute", "aVertexPosition")
+const modelTransform = simpleShader.getLocation("uniform", "uModelTransform")
 const pixelColor = simpleShader.getLocation("uniform", "uPixelColor")
 
-simpleShader.activate = (gl, color) => {
+simpleShader.activate = (gl, color, transform) => {
     gl.enableVertexAttribArray(vertexPosition)
     gl.vertexAttribPointer(vertexPosition, vertexBuffer.size, gl.FLOAT, false, 0, 0)
+    gl.uniformMatrix4fv(modelTransform, false, transform)
     gl.uniform4fv(pixelColor, color)
 }
 
@@ -24,5 +26,27 @@ engine.program(simpleShader)
     
 const vertexBuffer = new VertexBuffer("square")
 
-const object = new Object(simpleShader)
-object.render([1, 0, 0.5, 1])
+const objectOne = new Object(simpleShader)
+const objectTwo = new Object(simpleShader)
+
+const xform = mat4.create()
+
+objectOne.render([1, 0, 0.5, 1], (() => {
+    mat4.identity(xform)
+
+    mat4.translate(xform, xform, vec3.fromValues(-0.25, 0.25, 0.0))
+    mat4.rotateZ(xform, xform, 0.2)
+    mat4.scale(xform, xform, vec3.fromValues(1.2, 1.2, 1.0))
+
+    return xform
+})())
+
+objectTwo.render([1, 1, 1, 1], (() => {
+    mat4.identity(xform)
+
+    mat4.translate(xform, xform, vec3.fromValues(0.25, -0.25, 0.0))
+    mat4.rotateZ(xform, xform, -0.785)
+    mat4.scale(xform, xform, vec3.fromValues(0.4, 0.4, 1.0))
+    
+    return xform
+})())
